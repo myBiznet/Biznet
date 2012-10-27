3216db872122fd78548d574702c763ade64599da
Biznet <frankie.ngai@gmail.com>
2012-10-27 17:18:43 +0800

    Signed-off-by: Biznet <frankie.ngai@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biznet::Application.config.secret_token = 'ab6bb6e96ac222d0511b7a1cf7420cf94e5a5406b459b4dbde3cacd0c1a60cae20b7bb32d1b0758a1cdb0f6d5f4e980a1036e1de7935d9d223c7d55c3cbcdc49'
